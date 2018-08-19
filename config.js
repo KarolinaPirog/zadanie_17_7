4c0bea61d7322904b24c8c188b3576ca4c00873e
Karolina Piróg <karolina.swieca@wp.pl>
2018-08-19 21:19:46 +0200

    add passport authentication to google

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports ={
+    'GOOGLE_CLIENT_ID': '928049873150-bqs1jl43i2ve4p7hmndjba18kejpm7pt.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'EsjNzNp0YmaSGMnCcJ7Gv-4t',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
